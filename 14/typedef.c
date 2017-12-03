410ba3ceff7add808ff42f345144abad04568236
Silicon014 <kaguya.h@foxmail.com>
2017-12-03 10:24:53 +0800

    Upload

---
+++ 14/typedef.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+int main(void)
+{
+	typedef int type1, type2;
+	type2 a = 10;
+	printf("%d\n", a);
+	return 0;
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//变量能作为一维数组的元素数量么
+int main(void)
+{
+	int n = 5;
+	int num[n];
+	
+	return 0;
+}
